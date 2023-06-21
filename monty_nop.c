5dfcad01a7144e1a093f1d99dd74fc40d3573cff
Noransaber <noransaber685@gmail.com>
2023-06-21 00:38:23 -0700

    Code for two tasks

---
+++ monty_nop.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+
+/**
+ * nop - doesn't do anything
+ * @stack: Ptr stack.
+ * @line_num: where func call
+ */
+
+void nop(stack_t **stack, unsigned int line_num)
+{
+	(void) stack;
+	(void) line_num;
+}
