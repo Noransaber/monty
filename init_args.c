b27957225d0b52f79e3e01efd9583e34bc8c838f
Noransaber <noransaber685@gmail.com>
2023-06-21 00:07:05 -0700

    Code for two files

---
+++ init_args.c
@@ -0,0 +1,23 @@
+#include "monty.h"
+
+/**
+ * init_args - initiallize args to use
+ */
+void init_args(void)
+{
+	args = malloc(sizeof(arg_t));
+	if (args == NULL) /*check for error in malloc*/
+		malloc_error();
+
+	args->instruction = malloc(sizeof(instruction_t));
+	if (args->instruction == NULL)
+		melloc_error();
+
+	args->stream = NULL;
+	args->line = NULL;
+	args->head = NULL;
+	args->n_tokens = 0;
+	args->stack_len = 0;
+	args->stack = 1;
+	args->line_num = 0;
+}
