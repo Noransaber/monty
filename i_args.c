209212afe5384ac42825d1958876ad7cd7d6e4a9
Noransaber <noransaber685@gmail.com>
2023-06-22 01:23:01 -0700

    Code

---
+++ i_args.c
@@ -0,0 +1,24 @@
+#include "monty.h"
+
+/**
+ * i_args - define my args
+ */
+void i_args(void)
+{
+	args  = malloc(sizeof(arg_t));
+	if (arg == NULL)
+		malloc_m();
+
+	args->ins = malloc(sizeof(instruction_t));
+	if (args->ins == NULL)
+		malloc_m();
+
+	args->s = NULL;
+	args->head = NULL;
+	args->l = NULL;
+	args->n_tk = 0;
+	args->l_n = 0;
+	args->stack_l = 0;
+	args->stack = 1;
+}
+
